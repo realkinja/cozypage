59dfa305016eca08197445519675ffaa853f6234
realkinja <78376161+realkinja@users.noreply.github.com>
2026-08-04 21:25:34 +0200

    🚑️ Import mess ups

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Background from "@/components/background";
-import Button from "@/components/button";
 import styles from "./page.module.css";
 import IconDuckDuckGo from "@/components/icons/IconDuckDuckGo";
 import SearchBar from "@/components/search-bar";
