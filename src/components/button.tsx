f67b3e1e1769df6708de12baf5c5b8740b3e0149
realkinja <78376161+realkinja@users.noreply.github.com>
2026-08-05 13:32:04 +0200

    ✨ Add button component

---
+++ src/components/button.tsx
@@ -0,0 +1,41 @@
+import styles from "./css/button.module.css";
+
+type ButtonVariant = "default" | "icon" | "shortcut";
+
+interface ButtonProps extends React.PropsWithChildren {
+  className?: String;
+  variant?: ButtonVariant;
+  onClick?(event: React.MouseEvent<HTMLButtonElement>): void;
+}
+
+export default function Button({
+  variant = "default",
+  className,
+  children,
+  onClick,
+  ...props
+}: ButtonProps) {
+  let variantClass: string;
+
+  switch (variant) {
+    case "default":
+      variantClass = styles.primary;
+      break;
+    case "icon":
+      variantClass = styles.icon;
+      break;
+    case "shortcut":
+      variantClass = styles.shortcut;
+      break;
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className={`${variantClass} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
