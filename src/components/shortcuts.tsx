0e8e70fb715ebaab2ac049a32392744fcd868430
realkinja <78376161+realkinja@users.noreply.github.com>
2026-08-05 21:56:24 +0200

    ✨ Adding shortcuts

--- src/components/shortcuts.tsx
+++ src/components/shortcuts.tsx
@@ -2,23 +2,51 @@
 import { redirect } from "next/navigation";
 import Button from "./button";
 import styles from "./css/shortcuts.module.css";
-import stylesdialog from "@/components/css/dialog.module.css";
 import IconPlus from "./icons/IconPlus";
-import { useEffect, useState } from "react";
-import { getShortcutsFromStorage, Shortcut } from "@/lib/utils";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import {
+  getShortcutsFromStorage,
+  saveShortcutsToStorage,
+  Shortcut,
+} from "@/lib/utils";
 import {
   Dialog,
-  DialogOverlay,
   DialogContent,
   DialogTrigger,
   DialogTitle,
   DialogDescription,
   DialogHeader,
+  DialogFooter,
+  DialogClose,
 } from "./dialog";
+import Input from "./input";
+import Label from "./label";
 
-function AddShortcut() {
+function AddShortcut({
+  shortcuts,
+  setShortcuts,
+}: {
+  shortcuts: Shortcut[];
+  setShortcuts: Dispatch<SetStateAction<Shortcut[]>>;
+}) {
+  const [open, setOpen] = useState(false);
+  const handleSaveShortcut = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const formData = new FormData(form);
+    const shortcut: Shortcut = {
+      title: formData.get("title") as string,
+      url: formData.get("url") as string,
+    };
+
+    setOpen(false);
+    setShortcuts([...shortcuts, shortcut]);
+    saveShortcutsToStorage([...shortcuts, shortcut]);
+  };
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button className={styles.addShortcut} variant="shortcut">
           <IconPlus />
@@ -28,7 +56,38 @@ function AddShortcut() {
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Add shortcut</DialogTitle>
+          <DialogDescription>
+            Create a new shortcut here. Click 'Add' when you're done.
+          </DialogDescription>
         </DialogHeader>
+        <form onSubmit={handleSaveShortcut} className={styles.form}>
+          <div className={styles.fields}>
+            <div className={styles.fieldGroup}>
+              <Label htmlFor="title">Title</Label>
+              <Input
+                name="title"
+                id="title"
+                required
+                placeholder="i.e. Youtube"
+              />
+            </div>
+            <div className={styles.fieldGroup}>
+              <Label htmlFor="url">URL</Label>
+              <Input
+                name="url"
+                id="url"
+                required
+                placeholder="i.e. https://youtube.com"
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Save</Button>
+            <DialogClose asChild>
+              <Button variant="secondary">Close</Button>
+            </DialogClose>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
@@ -59,7 +118,9 @@ export default function Shortcuts() {
           {shortcut.title[0]}
         </Button>
       ))}
-      {shortcuts.length < 9 ? <AddShortcut /> : null}
+      {shortcuts.length < 9 ? (
+        <AddShortcut shortcuts={shortcuts} setShortcuts={setShortcuts} />
+      ) : null}
     </div>
   );
 }
