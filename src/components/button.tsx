80e60b0578115495a9135e01e7c411521ad23b23
realkinja <78376161+realkinja@users.noreply.github.com>
2026-08-06 13:22:55 +0200

    ♻️🎨 Refactor the way button does variants to not repeat myself

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,32 +2,22 @@ import styles from "./css/button.module.css";
 
 type ButtonVariant = "default" | "secondary" | "icon" | "shortcut";
 
+const buttonVariants = {
+  default: styles.default,
+  secondary: styles.secondary,
+  icon: styles.icon,
+  shortcut: styles.shortcut,
+} satisfies Record<ButtonVariant, string>;
+
 export default function Button({
   variant = "default",
   className,
   children,
   ...props
 }: React.ComponentProps<"button"> & { variant?: ButtonVariant }) {
-  let variantClass: string;
-
-  switch (variant) {
-    case "default":
-      variantClass = styles.primary;
-      break;
-    case "secondary":
-      variantClass = styles.secondary;
-      break;
-    case "icon":
-      variantClass = styles.icon;
-      break;
-    case "shortcut":
-      variantClass = styles.shortcut;
-      break;
-  }
-
   return (
     <button
-      className={`${variantClass}${className ? ` ${className}` : ""}`}
+      className={`${styles.button} ${buttonVariants[variant]}${className ? ` ${className}` : ""}`}
       {...props}
     >
       {children}
