fea640b008b5a4d44e664e082de7770d9d4a6fc0
realkinja <78376161+realkinja@users.noreply.github.com>
2026-08-06 18:43:40 +0200

    🔥 Cleanup after moving add shortcut component

--- src/components/shortcuts.tsx
+++ src/components/shortcuts.tsx
@@ -1,26 +1,11 @@
 "use client";
 import Button from "./button";
 import styles from "./css/shortcuts.module.css";
-import IconPlus from "./icons/IconPlus";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import {
   getShortcutsFromStorage,
   saveShortcutsToStorage,
   Shortcut,
-  shortcutFromForm,
 } from "@/lib/utils";
-import {
-  Dialog,
-  DialogContent,
-  DialogTrigger,
-  DialogTitle,
-  DialogDescription,
-  DialogHeader,
-  DialogFooter,
-  DialogClose,
-} from "./dialog";
-import Input from "./input";
-import Label from "./label";
 import Link from "next/link";
 import {
   ContextMenu,
@@ -42,74 +27,9 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from "./alert-dialog";
-
-function AddShortcut({
-  shortcuts,
-  setShortcuts,
-}: {
-  shortcuts: Shortcut[];
-  setShortcuts: Dispatch<SetStateAction<Shortcut[]>>;
-}) {
-  const [open, setOpen] = useState(false);
-  const handleSaveShortcut = (e) => {
-    e.preventDefault();
-
-    const shortcut = shortcutFromForm(e.target);
-
-    setOpen(false);
-    setShortcuts([...shortcuts, shortcut]);
-    saveShortcutsToStorage([...shortcuts, shortcut]);
-  };
-
-  return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button className={styles.addShortcut} variant="shortcut">
-          <IconPlus />
-          Add shortcut
-        </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Add shortcut</DialogTitle>
-          <DialogDescription>
-            Create a new shortcut here. Click 'Add' when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        <form onSubmit={handleSaveShortcut} className={styles.form}>
-          <div className={styles.fields}>
-            <div className={styles.fieldGroup}>
-              <Label htmlFor="title">Title</Label>
-              <Input
-                name="title"
-                id="title"
-                required
-                placeholder="i.e. Youtube"
-              />
-            </div>
-            <div className={styles.fieldGroup}>
-              <Label htmlFor="url">URL</Label>
-              <Input
-                name="url"
-                id="url"
-                required
-                placeholder="i.e. https://youtube.com"
-              />
-            </div>
-          </div>
-          <DialogFooter>
-            <Button type="submit">Save</Button>
-            <DialogClose asChild>
-              <Button variant="secondary">Close</Button>
-            </DialogClose>
-          </DialogFooter>
-        </form>
-      </DialogContent>
-    </Dialog>
-  );
-}
+import { useEffect, useState } from "react";
+import AddShortcut from "./add-shortcut";
 
 export default function Shortcuts() {
   const [shortcuts, setShortcuts] = useState(new Array<Shortcut>());
