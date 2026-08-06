db63b51386762560103a16f88415398727c03ad6
realkinja <78376161+realkinja@users.noreply.github.com>
2026-08-06 10:20:10 +0200

    🔥 Remove fonts that might not be needed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,24 +6,9 @@ const author = localFont({
   variable: "--font-author",
   src: [
     {
-      path: "../fonts/Author-Extralight.woff2",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Author-ExtralightItalic.woff2",
-      weight: "200",
-      style: "italic",
-    },
-    {
       path: "../fonts/Author-Light.woff2",
       weight: "300",
       style: "normal",
-    },
-    {
-      path: "../fonts/Author-LightItalic.woff2",
-      weight: "300",
-      style: "italic",
     },
     {
       path: "../fonts/Author-Regular.woff2",
@@ -31,19 +16,9 @@ const author = localFont({
       style: "normal",
     },
     {
-      path: "../fonts/Author-Italic.woff2",
-      weight: "400",
-      style: "italic",
-    },
-    {
       path: "../fonts/Author-Medium.woff2",
       weight: "500",
       style: "normal",
-    },
-    {
-      path: "../fonts/Author-MediumItalic.woff2",
-      weight: "500",
-      style: "italic",
     },
     {
       path: "../fonts/Author-Semibold.woff2",
@@ -51,19 +26,9 @@ const author = localFont({
       style: "normal",
     },
     {
-      path: "../fonts/Author-SemiboldItalic.woff2",
-      weight: "600",
-      style: "italic",
-    },
-    {
       path: "../fonts/Author-Bold.woff2",
       weight: "700",
       style: "normal",
-    },
-    {
-      path: "../fonts/Author-BoldItalic.woff2",
-      weight: "700",
-      style: "italic",
     },
   ],
 });
